183081b7d4df67cc49c971496a7f98b401c16235
mikeymop <midefran@gmail.com>
2019-03-27 14:49:52 -0400

    hw5 draft

---
+++ practice/DFS.c
@@ -0,0 +1,78 @@
+#include <stidio.h>
+#include <stdlib.h>
+
+struct node {
+    int item;
+    struct node* next;
+};
+
+struct node* createNode(int v);
+
+struct Graph {
+    int numVertices;
+    int* visited;
+    struct node** adjLists /* used to store 2dem array */
+}
+
+struct Graph* createGraph(int vertices)
+{
+    struct Graph* graph = malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+ 
+    graph->adjLists = malloc(vertices * sizeof(struct node*));
+    
+    graph->visited = malloc(vertices * sizeof(int));
+ 
+    int i;
+    for (i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest)
+{
+    // Add edge from src to dest
+    struct node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+ 
+    // Add edge from dest to src
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+void printGraph(struct Graph* graph)
+{
+    int v;
+    for (v = 0; v < graph->numVertices; v++)
+    {
+        struct node* temp = graph->adjLists[v];
+        printf("\n Adjacency list of vertex %d\n ", v);
+        while (temp)
+        {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+void DFS(struct Graph* graph, int vertex) {
+        struct node* adjList = graph->adjLists[vertex];
+        struct node* temp = adjList;
+        
+        graph->visited[vertex] = 1;
+        printf("Visited %d \n", vertex);
+    
+        while(temp!=NULL) {
+            int connectedVertex = temp->vertex;
+        
+            if(graph->visited[connectedVertex] == 0) {
+                DFS(graph, connectedVertex);
+            }
+            temp = temp->next;
+        }       
+}
