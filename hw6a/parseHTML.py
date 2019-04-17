6e6ecba69f7cfae7d5153755206f903f99ddd7a5
mikeymop <midefran@gmail.com>
2019-04-17 17:34:01 -0400

    cleaned up output

--- hw6a/parseHTML.py
+++ hw6a/parseHTML.py
@@ -5,6 +5,8 @@ def getDate():
     curtime = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
     return curtime
 
+garbagetags = ["Issue", "Volume", "Price", "Chg", "% Chg", " "]
+
 arg = sys.argv
 
 try:
@@ -26,18 +28,26 @@ cdom = xml.parse(ifile)
 
 table = cdom.getElementsByTagName("table")
 
+csvrow = ""
+
 for row in table:
+    result = ""
     tr = row.getElementsByTagName("tr")
 
     for cell in tr:
-        incel = cell.getElementsByTagName("td")
+        td = cell.getElementsByTagName("td")
+        i = 1
+        for element in td[:]:
+            if (element.nodeType != element.TEXT_NODE):
+                for a in element.getElementsByTagName("a"):
+                    name = str(a.firstChild.nodeValue).split("\n")
+                    if (len(name) > 1):
+                        print(str(repr(name[0])).strip('\''))
 
-        for data in incel:
-            if data.nodeType != data.TEXT_NODE:
-                for a in data.getElementsByTagName("a"):
-                    name = a.firstChild
-                    print(name.nodeValue)
-            print(data.firstChild.nodeValue)
+                values = str(element.firstChild.nodeValue).strip("\n")
 
-#print(data)
+                if(len(values) > 0 and str(values) != "None" and len(values) > 1):
+                    if(values not in garbagetags and " " not in values):
+                        print(len(values), values)
+
 cdom.unlink()
