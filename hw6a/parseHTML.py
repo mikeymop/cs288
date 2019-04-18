1c19492a417659b3452189286a9b3afd70b33d15
mikeymop <midefran@gmail.com>
2019-04-18 13:16:47 -0400

    nearly finished

--- hw6a/parseHTML.py
+++ hw6a/parseHTML.py
@@ -3,7 +3,7 @@ import xml.dom.minidom as xml
 
 def getDate():
     curtime = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
-    return curtime
+    return str(curtime)
 
 garbagetags = ["Issue", "Volume", "Price", "Chg", "% Chg", " "]
 
@@ -13,7 +13,14 @@ try:
     filename = arg[1]
 except:
     filename = str(getDate())
-    print("File not found, assuming: ")
+    print("File not found, assuming: " + filename)
+
+def writeLine(csvrow, ofile):
+    i = 0
+    while i < 6:
+        ofile.write(str(csvrow[i]))
+        i += 1
+    ofile.write("\n")
 
 """
 filename +=".html"
@@ -26,9 +33,12 @@ except:
 ifile = open(filename, "r")
 cdom = xml.parse(ifile)
 
+ofile = open(getDate()+".csv", "w")
+
 table = cdom.getElementsByTagName("table")
 
-csvrow = ""
+csvrow = []
+#csvrow[0] = "nasdaq"
 
 for row in table:
     result = ""
@@ -36,18 +46,46 @@ for row in table:
 
     for cell in tr:
         td = cell.getElementsByTagName("td")
-        i = 1
+        csvrow.append("nasdaq")
+
         for element in td[:]:
             if (element.nodeType != element.TEXT_NODE):
                 for a in element.getElementsByTagName("a"):
                     name = str(a.firstChild.nodeValue).split("\n")
                     if (len(name) > 1):
-                        print(str(repr(name[0])).strip('\''))
+                        symbol = str(repr(name[0])).strip('\'').split(' ')
+                        symbol = str(repr(symbol[-1])).strip('\'')
+                        name = name[0]
 
-                values = str(element.firstChild.nodeValue).strip("\n")
+                        csvrow.append(str(symbol))
+                        csvrow.append(str(name[:-6]))
+                        #continue
+                        #print(name[:-6])
+                        #print(symbol)
+                        #print(csvrow)
+                        #print("\n")
+                #continue
+            values = str(element.firstChild.nodeValue).strip("\n")
 
-                if(len(values) > 0 and str(values) != "None" and len(values) > 1):
-                    if(values not in garbagetags and " " not in values):
-                        print(len(values), values)
-
+            if(str(values) != "None"):
+                if(values not in garbagetags and " " not in values):
+                    if('$' in str(values)):
+                        values = values.replace("$", "")
+                    if(',' in str(values)):
+                        values = values.replace(",", "")
+                    csvrow.append(values)
+                    #print(csvrow)
+                    #continue
+        #if(len(csvrow) == 6):
+        #csvrow.remove(csvrow[1])
+        if(len(csvrow) > 8):
+            csvrow.pop(4)
+            csvrow.pop(1)
+            csvrow.pop(6)
+            writeLine(csvrow, ofile)
+            print(csvrow)
+            print("\n")
+        csvrow.clear()
+        continue
+ofile.close()
 cdom.unlink()
